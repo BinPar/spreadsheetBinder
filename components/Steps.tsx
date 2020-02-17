9902597bae8eaec39db83f80bc668ea5f93ece5a
drnachio <nacho@binpar.com>
2020-02-17 09:52:03 +0100

    Fixes

---
+++ components/Steps.tsx
@@ -0,0 +1,75 @@
+import React from 'react';
+import { makeStyles, Theme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import NoSsr from '@material-ui/core/NoSsr';
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps): JSX.Element {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <Typography
+      component="div"
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box p={3}>{children}</Box>}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme: Theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+}));
+
+export default function SimpleTabs(): JSX.Element {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (_: React.ChangeEvent<{}>, newValue: number): void => {
+    setValue(newValue);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+        >
+          <Tab label="Celdas a puntuar" />
+          <Tab label="Celdas a comparar" />
+          <Tab label="Resultado" />
+        </Tabs>
+      </AppBar>
+      <TabPanel value={value} index={0}>
+        <NoSsr>
+          Item One
+        </NoSsr>
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        Item Two
+      </TabPanel>
+      <TabPanel value={value} index={2}>
+        Item Three
+      </TabPanel>
+    </div>
+  );
+}
